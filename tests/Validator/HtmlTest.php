0034794951c47004073e3ddbfb0a61f07e6abffc
Anatolii Demianenko <a.s.demyanenko@gmail.com>
2019-04-28 11:10:12 +0300

    travis ci test

---
+++ tests/Validator/HtmlTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use WinterUni\GoogleDoc\Validator\Html;
+
+class HtmlTest extends TestCase
+{
+    /** @var Html */
+    private $htmlValidator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->htmlValidator = new Html();
+    }
+
+    public function testTest()
+    {
+        $this->assertFalse(true);
+    }
+}
